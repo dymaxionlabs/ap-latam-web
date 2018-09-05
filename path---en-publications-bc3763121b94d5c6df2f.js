244cef44add1ff1eb726adc260cb5bfa410b5df5
Damián Silvani <munshkr@gmail.com>
2018-09-05 12:36:35 -0300

    Updates

--- path---en-publications-bc3763121b94d5c6df2f.js
+++ path---en-publications-bc3763121b94d5c6df2f.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xe4a43b470a8a],{756:function(n,a){n.exports={pathContext:{slug:"/en/publications/",langKey:"en"}}}});
+webpackJsonp([0xe4a43b470a8a],{759:function(n,a){n.exports={pathContext:{slug:"/en/publications/",langKey:"en"}}}});
 //# sourceMappingURL=path---en-publications-bc3763121b94d5c6df2f.js.map
--- path---es-publications-0db5af3b8fa2a478f960.js
+++ path---es-publications-0db5af3b8fa2a478f960.js
@@ -1,2 +1,2 @@
-webpackJsonp([6852714446521],{761:function(e,n){e.exports={pathContext:{slug:"/es/publications/",langKey:"es"}}}});
+webpackJsonp([6852714446521],{764:function(e,n){e.exports={pathContext:{slug:"/es/publications/",langKey:"es"}}}});
 //# sourceMappingURL=path---es-publications-0db5af3b8fa2a478f960.js.map
